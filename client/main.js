617858befdc091d1d14b38fb18a68d95f49a8d00
Juliano S. De Cezaro <julianodecezaro@gmail.com>
2016-05-11 18:01:14 -0300

    Adding Semantic UI

--- client/main.js
+++ client/main.js
@@ -1,3 +1,18 @@
+/*Meteor.startup(() => {
+    WebFontConfig = {
+        google: { families: [ 'Roboto Slab:700,400:latin', 'Oswald:400', 'Mouse Memoirs' ] }
+    };
+    (function() {
+        var wf = document.createElement('script');
+        wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+        wf.type = 'text/javascript';
+        wf.async = 'true';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(wf, s);
+        //console.log("async fonts loaded", WebFontConfig);
+    })();
+});*/
+
 References = new Mongo.Collection('references');
 
 Template.references.helpers({
@@ -23,6 +38,10 @@ Template.references.events({
 
 Template.registerHelper('formatDate', function(date) {
 	return moment(date).format('DD/MM/YYYY HH:mm:ss');
+});
+
+Template.registerHelper('btnDflt', function(_id, _class, _icon, _text) {
+    return '<div id="'+_id+'" type="submit" class="'+_class+'"><div class="icon"><i class="mdi mdi-'+_icon+'"></i></div><div class="text">'+_text+'</div></div>';
 });
 
 Router.configure({
