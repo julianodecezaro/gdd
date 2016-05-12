9e913f0d5c33ddc7b223f4fa996435964320b91d
Juliano De Cezaro <Juliano De Cezaro>
2016-05-11 23:14:31 -0300

    Testing semantic-ui

--- client/main.js
+++ client/main.js
@@ -15,58 +15,66 @@
 
 References = new Mongo.Collection('references');
 
+Template.menu.onRendered(function () {
+    this.$(".menu").menu();
+});
+Template.references.onRendered(function () {
+    this.$(".dropdown").dropdown();
+    this.$(".checkbox").checkbox();
+});
+
 Template.references.helpers({
-	references: function(){
-		return References.find();
-	}
+    references: function () {
+        return References.find();
+    }
 });
 
 Template.references.events({
-	'submit .frm-references': function(event){		
-		var name = event.target.name.value;
-		References.insert({
-			name: name,
-			createdAt: new Date()
-		});
-		event.target.name.value = '';
-		return false;
-	},
-	'click #delete-ref': function(){
-		References.remove(this._id);
-	}
+    'submit .frm-references': function (event) {
+        var name = event.target.name.value;
+        References.insert({
+            name: name
+            , createdAt: new Date()
+        });
+        event.target.name.value = '';
+        return false;
+    }
+    , 'click #delete-ref': function () {
+        References.remove(this._id);
+    }
 });
 
-Template.registerHelper('formatDate', function(date) {
-	return moment(date).format('DD/MM/YYYY HH:mm:ss');
+Template.registerHelper('formatDate', function (date) {
+    return moment(date).format('DD/MM/YYYY HH:mm:ss');
 });
 
-Template.registerHelper('btnDflt', function(_id, _class, _icon, _text) {
-    return '<div id="'+_id+'" type="submit" class="'+_class+'"><div class="icon"><i class="mdi mdi-'+_icon+'"></i></div><div class="text">'+_text+'</div></div>';
+Template.registerHelper('btnDflt', function (_id, _class, _icon, _text) {
+    return '<div id="' + _id + '" type="submit" class="' + _class + '"><div class="icon"><i class="mdi mdi-' + _icon + '"></i></div><div class="text">' + _text + '</div></div>';
 });
 
 Router.configure({
-	layoutTemplate: 'appLayout'
+    layoutTemplate: 'appLayout'
 });
 
-Router.onBeforeAction(function() {
-	//if (! Meteor.userId()) {
-	/*if (!this.userId) {
-		this.render('login');
-	} else {*/
-		this.next();
-	//}
+Router.onBeforeAction(function () {
+    //if (! Meteor.userId()) {
+    /*if (!this.userId) {
+    	this.render('login');
+    } else {*/
+    this.next();
+    //}
 });
 
 Router.route('/', function () {
-	this.render('home');
+    this.render('home');
 });
 
 Router.route('/references', function () {
-	this.render('references');
+    this.render('references');
 });
 
-$(document).ready(function(){
-	$(document).on('click', '#save-ref', function(){
-		$('.frm-references').submit();
-	});
+$(document).ready(function () {
+    $(document).on('click', '#save-ref', function () {
+        $('.frm-references').submit();
+    });
 });
